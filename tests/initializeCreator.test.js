af7009d6ffe8948d2754d4009f86ad5c8dd8c25e
makarychev <3645723+makarychev@users.noreply.github.com>
2024-03-26 16:31:23 +0200

    add inititalize creator test

---
+++ tests/initializeCreator.test.js
@@ -0,0 +1,131 @@
+const anchor = require("@coral-xyz/anchor");
+const {
+  createMint,
+  mintTo,
+  getOrCreateAssociatedTokenAccount
+} = require('@solana/spl-token');
+const os = require('os');
+const assert = require('assert');
+
+const TREASURY_PREFIX = 'treasury';
+const CREATOR_PREFIX = 'creator';
+
+describe("donaproto", () => {
+  const homedir = os.homedir();
+  process.env.ANCHOR_WALLET = `${homedir}/.config/solana/id.json`;
+  const program = anchor.workspace.Donaproto;
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+  const connection = provider.connection;
+  const { payer } = program.provider.wallet
+  const donationMintAuthority = payer;
+  const donationMintDecimals = 6;
+  let donationMintPubKey, rewardsMintPubKey
+  const rewardMintAuthority = payer;
+  const rewardMintDecimals = 9;
+  const donationProtocolData = anchor.web3.Keypair.generate();
+  const minAmountToEarn = new anchor.BN(1000);
+  const creatorWallet = anchor.web3.Keypair.generate();
+
+  before(async () => {
+    donationMintPubKey = await createMint(
+      provider.connection,
+      payer,
+      donationMintAuthority.publicKey,
+      null,
+      donationMintDecimals
+    );
+
+    rewardsMintPubKey = await createMint(
+      connection,
+      payer,
+      rewardMintAuthority.publicKey,
+      null,
+      rewardMintDecimals
+    )
+
+    const [treasuryOwnerPK, treasuryOwnerBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(TREASURY_PREFIX),
+        donationProtocolData.publicKey.toBuffer(),
+      ],
+      program.programId,
+    );
+
+    const treasuryTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      rewardsMintPubKey,
+      treasuryOwnerPK,
+      allowOwnerOffCurve = true
+    )
+
+    await program.rpc.initializeDonationProtocol(
+      minAmountToEarn,
+      treasuryOwnerBump,
+      {
+        accounts: {
+          donationProtocolData: donationProtocolData.publicKey,
+          treasury: treasuryTokenAccount.address,
+          treasuryOwner: treasuryOwnerPK,
+          treasuryMint: rewardsMintPubKey,
+          donationMint: donationMintPubKey,
+          payer: payer.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+        signers: [donationProtocolData],
+      }
+    );
+  })
+
+  it("initializes creator data", async () => {
+    const creatorDonationTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      donationMintPubKey,
+      creatorWallet.publicKey
+    )
+
+    await mintTo(
+      connection,
+      payer,
+      donationMintPubKey,
+      creatorDonationTokenAccount.address,
+      donationMintAuthority,
+      10000000000
+    )
+
+    const [creatorDataPubkey, creatorDataBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(CREATOR_PREFIX),
+        donationProtocolData.publicKey.toBuffer(),
+        creatorWallet.publicKey.toBuffer(),
+      ],
+      program.programId,
+    );
+
+    const tx = await program.rpc.initializeCreator(
+      {
+        accounts: {
+          creatorData: creatorDataPubkey,
+          donationProtocol: donationProtocolData.publicKey,
+          creatorWalletAddress: creatorWallet.publicKey,
+          payer: payer.publicKey,
+          donationMint: donationMintPubKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      },
+    );
+    console.log("Your transaction signature", tx);
+
+    // check data
+    const onchainCreatorData = await program.account.creatorData.fetch(creatorDataPubkey);
+    assert.ok(onchainCreatorData.donationProtocol.equals(donationProtocolData.publicKey));
+    assert.equal(onchainCreatorData.donationsCreatedCount, 0);
+    assert.equal(onchainCreatorData.donationsClosedCount, 0);
+    assert.equal(onchainCreatorData.totalAmountReceived, 0);
+    assert.equal(onchainCreatorData.totalAmountCollecting, 0);
+  });
+});
